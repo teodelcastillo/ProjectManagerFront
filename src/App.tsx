f920433c112c282c76ca3eb668607a2e2e801876
teodelcastillo <teodorodelcastillop@gmail.com>
2023-10-27 12:55:21 -0300

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import {Button} from '@chakra-ui/react'
+import { Grid, GridItem } from "@chakra-ui/react"
+
 
 function App() {
 
 
   return (
-   <Button >BOTONAZO</Button>
+    <Grid
+    templateAreas={`"header"
+                    "main"
+                    "footer"`}
+    gridTemplateRows={'50px 1fr 30px'}
+    gridTemplateColumns={'1fr'}
+    h='200px'
+    gap='1'
+    color='blackAlpha.700'
+    fontWeight='bold'
+    height='100vh'
+  >
+    <GridItem pl='2' bg='orange.300' area={'header'}>
+      Header
+    </GridItem>
+  
+    <GridItem pl='2' bg='green.300' area={'main'}>
+      Main
+    </GridItem>
+    <GridItem pl='2' bg='blue.300' area={'footer'}>
+      Footer
+    </GridItem>
+  </Grid>
   )
 }
 
