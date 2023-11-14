67552e30578d9e5c34531918c354ba36b37e9135
teodelcastillo <teodorodelcastillop@gmail.com>
2023-11-14 19:55:36 -0300

    projects with appointments not done

--- src/hooks/useProjectWithAppointments.ts
+++ src/hooks/useProjectWithAppointments.ts
@@ -1,6 +1,6 @@
 import Project from "../models/Projects";
 import useData from "./useData";
 
-const useProjectWithAppointments = () => useData<Project>('/projects/with-appointments/')
+const useProjectWithAppointments = () => useData<Project>('/projects/workbench/')
 
 export default useProjectWithAppointments
